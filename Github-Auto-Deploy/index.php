3b659667fab15384f32177e7a9d84540045e7c1b
ΚΣΣGΔΠ ΒRΘΨΠ <brown.keegan@gmail.com>
2014-05-16 14:19:55 -0500

    ignore

--- Github-Auto-Deploy/index.php
+++ Github-Auto-Deploy/index.php
@@ -14,10 +14,10 @@ if ( !!$payload ) {
 	$logso .= "\n". $payload['head_commit']['timestamp'];
 }
 
-$fetch = shell_exec('cd ../private && ./gitfetch.sh 2>&1');
+$fetch = shell_exec('cd /var/www/clients/client1/web5/private && ./gitfetch.sh 2>&1');
 $output = shell_exec('git log --name-status HEAD^..HEAD');
 
-//echo "\n\n <br />".$fetch."\n\n <br />";
+echo "\n\n <br />".$fetch."\n\n <br />";
 
 $logso .= "\n" . $output;
 file_put_contents('logs/github.txt', print_r($logso, TRUE) );
