ef127cd13507b956f3bb6327595d115ef5d7cfbf
Cyril Celestine <cyril.celestine@gmail.com>
2014-05-22 17:05:20 -0500

    moving seattle gig changes to pdxgig

--- offers/PDXgig/js/main.js
+++ offers/PDXgig/js/main.js
@@ -1,114 +1,59 @@
-if ( !!document.createElement('canvas').getContext && !!document.createElement('canvas').getContext('2d')) {
-        //TIMELINES FOR MODERN BROWSERS
+if ( !! document.createElement('canvas').getContext && !! document.createElement('canvas').getContext('2d')) {
+    //TIMELINES FOR MODERN BROWSERS
     $(function() {
-        var mainw = $("#stars").width(),
-            mainh = $("#stars").height(),
-            midX = Math.floor(mainw / 2),
-            midY = Math.floor(mainh / 2);
+        var particle = $(".particle");
 
-        var stage = new Kinetic.Stage({
-            container: "stars",
-            width: mainw,
-            height: mainh
-        });
+        function getEachParticle() {
+            particle.each(particleCoords);
+        }
 
-        var layer = new Kinetic.Layer();
-        var topLayer = new Kinetic.Layer();
-        var group = new Kinetic.Group();
+        function particleCoords(i, ele) {
+            var rnd = Math.random();
+            var fxr = $(".row.hero").width();
+            var txl = 0 + (Math.random() * -200);
+            var fyr = 0 + (Math.random() * 165);
+            var tyl = 250 + (Math.random() * 300);
 
-        var _baseRad = 2;
-        var _num = 50;
+            var angleDegL = Math.atan2(fyr - tyl, fxr - txl) * 180 / Math.PI;
 
-        var tweens = [];
-        var _tmo = null;
+            randNumLeft(i, ele, txl, fxr, angleDegL, tyl, fyr);
+        }
 
-        // $(window).on( {
-        // 	click: function (e) {
-        // 		rndComet();
-        // 	}
-        // });
-
-        rndComet();
-
-        function rndComet() {
-            tweenComet({
-                x: /* Math.round( Math.random() )* */ mainw,
-                y: Math.round((Math.random() * 250))
+        function randNumLeft(i, comet, txl, fxr, angleDegL, tyl, fyr) {
+            TweenMax.fromTo(comet, 1, {
+                x: fxr,
+                y: fyr,
+                rotation: angleDegL,
+                opacity: 0.6,
+                scaleY: 0.6
+            }, {
+                x: txl,
+                y: tyl,
+                rotation: angleDegL,
+                opacity: 0,
+                scaleY: 0,
+                ease: Power4.easeOut,
+                delay: 3,
+                onComplete: particleCoords,
+                onCompleteParams: [i, comet]
             });
         }
 
-        function tweenComet(startpos) {
-            var dots = group.getChildren();
-            var toX = (startpos.x > 1) ? 0 : mainw;
-            var toY = Math.round(Math.random() * 100) + 200 + startpos.y; //Math.round( (Math.random()*mainh) );
-            var _yrnd = Math.random();
-            var time = 2;
+        getEachParticle();
 
-            //!! window.console && console.log(startpos, toX, toY)
-
-            for (var i = 0; i < dots.length; i++) {
-                dots[i].setX(startpos.x);
-                dots[i].setY(startpos.y);
-                dots[i].setOpacity(0.3);
-                tweens[i] = TweenMax.to(dots[i], time, {
-                    setOpacity: 0,
-                    overwrite: "all",
-                    setX: toX,
-                    setY: toY,
-                    delay: i * time * 0.0005,
-                    ease: Power4.easeOut
-                });
-            }
-            if ( !! _tmo) {
-                clearTimeout(_tmo);
-            }
-            _tmo = setTimeout(rndComet, time * 500 + Math.floor(Math.random() * 2500));
-        }
-
-        for (i = 0; i < _num; i++) {
-            var radius = _baseRad + 1 - _baseRad * (i / (_num));
-    		var circle = new Kinetic.Rect({
-    			x: 0,
-    			y: 0,
-    			width: radius,
-    			height: radius,
-    			fill: '#FFFFFF',
-    			strokeWidth: 0
-    		});
-            group.add(circle);
-        }
-        stage.add(layer);
-        layer.add(group);
-        stage.add(topLayer);
-        //group.setOffset(0, 0);
-        TweenLite.ticker.addEventListener("tick", go);
-
-        var lastRot = 0;
-        group.setPosition(0, 0);
-
-        function go() {
-            stage.clear();
-            group.draw();
-            topLayer.draw();
-        }
-
-        // var tl = new TimelineMax();
-        // var row = $(".row");
-
-        //tl.staggerTo(row, 1, {opacity:1}, 0.4);
-        //
-        
         $('.sec1').waypoint(function() {
             $('.one').animate({
-                 width: "100%"
-            }, 500 );
+                width: "100%"
+            }, 500);
             $('.two').animate({
-                 width: "100%"
-            }, 900 );
+                width: "100%"
+            }, 900);
             $('.three').animate({
-                 width: "100%"
-            }, 1300 );
-        }, { offset: 'bottom-in-view'});
+                width: "100%"
+            }, 1300);
+        }, {
+            offset: 'bottom-in-view'
+        });
 
     });
 }
