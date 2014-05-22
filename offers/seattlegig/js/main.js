7dec11e6d9c3e9eec76816f6f3811d8cc2f28ed2
Cyril Celestine <cyril.celestine@gmail.com>
2014-05-22 15:40:27 -0500

    ybend?

--- offers/seattlegig/js/main.js
+++ offers/seattlegig/js/main.js
@@ -10,8 +10,8 @@ if ( !! document.createElement('canvas').getContext && !! document.createElement
         function particleCoords(i, ele) {
             var rnd = Math.random();
             var fxl = $(".row.hero").width()+175;
-            var txl = -175;
-            var yBend = 320; //150+(Math.random()*320);
+            var txl = 175;
+            var yBend = 150+(Math.random()*320);
 
             var angleDegL = Math.atan2(165 - yBend, fxl - txl) * 180 / Math.PI;
             randNumLeft(i, ele, txl, fxl, angleDegL, yBend);
