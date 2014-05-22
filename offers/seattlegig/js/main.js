a1cb08d085c55f85294ab168deb9a10b4528e478
Cyril Celestine <cyril.celestine@gmail.com>
2014-05-22 15:24:51 -0500

    making changes

--- offers/seattlegig/js/main.js
+++ offers/seattlegig/js/main.js
@@ -2,12 +2,12 @@ if ( !!document.createElement('canvas').getContext && !!document.createElement('
         //TIMELINES FOR MODERN BROWSERS
     $(function() {
         var particle = $(".particle");
-        function getEachParticle(){
-            particle.each(particleCoords);
-        }
-        function particleCoords(i, ele){
+        // function getEachParticle(){
+        //     particle.each(particleCoords);
+        // }
+        function particleCoords(i, particle){
             var rnd = Math.random();
-            var fxl = 462;
+            var fxl = 900;
             var txl = -400+(Math.random()*1500);
             var yBend = -100;
 
@@ -18,13 +18,16 @@ if ( !!document.createElement('canvas').getContext && !!document.createElement('
             }
 
             var angleDegL = Math.atan2(165 - yBend, fxl - txl) * 180 / Math.PI;
+
             randNumLeft(i, ele, txl, fxl, angleDegL, yBend);
         }
+
         function randNumLeft( i, comet, txl, fxl, angleDegL, yBend ){
             TweenMax.fromTo(comet, 0.6+(Math.random()*1), {x:fxl, y:165, rotation:angleDegL, opacity:0.1, scaleY:0.1}, {x:txl, y:yBend, rotation: angleDegL, opacity: 0.6, scaleY: 0.5, ease:Power4.easeOut, onComplete:particleCoords, onCompleteParams:[i,comet] });
         }
         
-        getEachParticle();
+        // getEachParticle();
+        particleCoords();
         
         $('.sec1').waypoint(function() {
             $('.one').animate({
