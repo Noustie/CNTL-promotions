f8d2b023c30777ddacc77bcec50215adbace6fe2
Cyril Celestine <cyril.celestine@gmail.com>
2014-05-22 15:26:58 -0500

    particle system

--- offers/seattlegig/js/main.js
+++ offers/seattlegig/js/main.js
@@ -7,7 +7,7 @@ if ( !!document.createElement('canvas').getContext && !!document.createElement('
         // }
         function particleCoords(i, particle){
             var rnd = Math.random();
-            var fxl = 900;
+            var fxl = 462;
             var txl = -400+(Math.random()*1500);
             var yBend = -100;
 
@@ -18,10 +18,8 @@ if ( !!document.createElement('canvas').getContext && !!document.createElement('
             }
 
             var angleDegL = Math.atan2(165 - yBend, fxl - txl) * 180 / Math.PI;
-
-            randNumLeft(i, ele, txl, fxl, angleDegL, yBend);
+            randNumLeft(i, particle, txl, fxl, angleDegL, yBend);
         }
-
         function randNumLeft( i, comet, txl, fxl, angleDegL, yBend ){
             TweenMax.fromTo(comet, 0.6+(Math.random()*1), {x:fxl, y:165, rotation:angleDegL, opacity:0.1, scaleY:0.1}, {x:txl, y:yBend, rotation: angleDegL, opacity: 0.6, scaleY: 0.5, ease:Power4.easeOut, onComplete:particleCoords, onCompleteParams:[i,comet] });
         }
