ae7fef03e6042673ec864397063aae4e9af44660
Cyril Celestine <cyril.celestine@gmail.com>
2014-06-10 15:28:50 -0500

    animation updates

--- offers/smart/js/main.js
+++ offers/smart/js/main.js
@@ -202,8 +202,8 @@ $(function(){
 		tl_demo.to( $(".lady"), 0.5, { left:"-200px", scale: 1.3, alpha:0, ease: Power2.easeInOut}, 0)
 		 		.to( $(".anim2"), 0.5, { left:"700px", scale: 1.3, alpha:0, ease: Power2.easeInOut}, 0)
 		 		
-		 		.staggerTo(snipePanels, 0.45,{ x:700, rotationX:90, scale:0.5, ease:Power2.easeOut}, 0.01, 0.7)
-				.to($(".limited-time-offer-snipe a"), 0.7,{ opacity:0, ease:Power2.easeInOut}, 0.7)
+		 		.staggerTo(snipePanels, 0.5,{rotationX:90, scale:0.5, ease:Power2.easeIn}, 0.01, 0)
+				.to($(".limited-time-offer-snipe a"), 0.5,{ opacity:0, ease:Power2.easeInOut}, 0)
 
 				.to( $(".lady, .anim2"), 0.1, { display:"none"}, 0.7)
 				.to($(".roomblur"), 0.5, { alpha:0 }, 0.3)
