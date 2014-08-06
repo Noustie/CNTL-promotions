da0cbe65c82c2269818dba6e155fa9a418707fd8
Cyril Celestine <cyril.celestine@gmail.com>
2014-08-06 11:21:40 -0500

    adjusting parallax stuff

--- offers/internet/js/main.js
+++ offers/internet/js/main.js
@@ -1,3 +1,20 @@
 $(function(){
-	
+
+	var bg = $("body");
+
+	$(window).scroll( function() { 
+		var fromtheTop = $(this).scrollTop(); 
+		// Calculate distance scrolled 
+		if (fromtheTop > 0) { 
+			// Make sure it's never less than 0 
+			var newPos = ( (fromtheTop * 0.5) + 0); 
+			//Ratio here - change 
+			if ($('html').hasClass('csstransforms3d')) {
+				TweenMax.to(bg, 0.2, {backgroundPosition:'center -'+newPos+'px'});						
+			}
+		} else { 
+			// If it's less than 0, reset to original position 
+			TweenMax.to(bg, 0.01, {backgroundPosition:'center 0px'});						
+		}
+	});
 });
