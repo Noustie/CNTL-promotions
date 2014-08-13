6fdd23df3e446a018f8e8419c4f54795eb3382c5
Cyril Celestine <cyril.celestine@gmail.com>
2014-08-13 14:36:46 -0500

    tweaks

--- offers/internet/js/main.js
+++ offers/internet/js/main.js
@@ -7,14 +7,14 @@ $(function(){
 		// Calculate distance scrolled 
 		if (fromtheTop > 0) { 
 			// Make sure it's never less than 0 
-			var newPos = ( (fromtheTop * 0.1) + 0); 
+			var newPos = ( (fromtheTop * 0.08) + 0); 
 			//Ratio here - change 
 			if ($('html').hasClass('csstransforms3d')) {
-				TweenMax.to(bg, 0.2, {backgroundPosition:'center '+newPos+'px'});						
+				TweenMax.to(bg, 0.1, {backgroundPosition:'center '+newPos+'px'});						
 			}
 		} else { 
 			// If it's less than 0, reset to original position 
-			TweenMax.to(bg, 0.01, {backgroundPosition:'center 0px'});						
+			TweenMax.to(bg, 0.1, {backgroundPosition:'center 0px'});						
 		}
 	});
 });
