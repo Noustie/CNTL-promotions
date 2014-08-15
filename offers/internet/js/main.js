8ddd04541501bf592982c0894b7ccb6defd141ae
Cyril Celestine <cyril.celestine@gmail.com>
2014-08-15 11:36:11 -0500

    clean up on internet page

--- offers/internet/js/main.js
+++ offers/internet/js/main.js
@@ -1,60 +1,24 @@
 $(function(){
 
 	var bg = $("body");
+	var newPos = 0;
+	var fromtheTop = $(window).scrollTop();
 
-
-
-
-	// function x(){
-
-	// }
-
-
-
-
-
-
-
-	$(window).scroll( function() { 
-		var fromtheTop = $(this).scrollTop();
-		console.log(fromtheTop);
-
-
-		// Calculate distance scrolled 
+	function parallax(){
 		if (fromtheTop > 0) { 
-			// Make sure it's never less than 0 
-			var newPos = ( (fromtheTop * 0.08) + 0); 
-			//Ratio here - change 
 			if ($('html').hasClass('csstransforms3d')) {
-				TweenMax.to(bg, 0.1, {backgroundPosition:'center '+newPos+'px'});						
+				TweenMax.to(bg, 0.2, {backgroundPosition:'center '+newPos+'px'});						
 			}
 		} else { 
-			// If it's less than 0, reset to original position 
 			TweenMax.set(bg, {backgroundPosition:'center 0px'});						
 		}
+	}
+
+	TweenMax.ticker.addEventListener("tick", parallax);
+
+	$(window).scroll( function() { 
+		fromtheTop = $(this).scrollTop();
+		newPos = ( (fromtheTop * 0.25) + 0); 
 	});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 });
