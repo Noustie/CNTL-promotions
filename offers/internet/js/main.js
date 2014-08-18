49fdc5553f5a5b0b7b9ddabffbcfd8da43c01446
Cyril Celestine <cyril.celestine@gmail.com>
2014-08-18 10:30:35 -0500

    no parallax

--- offers/internet/js/main.js
+++ offers/internet/js/main.js
@@ -1,24 +1,24 @@
-$(function(){
+// $(function(){
 
-	var bg = $("body");
-	var newPos = 0;
-	var fromtheTop = $(window).scrollTop();
+// 	var bg = $("body");
+// 	var newPos = 0;
+// 	var fromtheTop = $(window).scrollTop();
 
-	function parallax(){
-		if (fromtheTop > 0) { 
-			if ($('html').hasClass('csstransforms3d')) {
-				TweenMax.to(bg, 0.2, {backgroundPosition:'center '+newPos+'px'});						
-			}
-		} else { 
-			TweenMax.set(bg, {backgroundPosition:'center 0px'});						
-		}
-	}
+// 	function parallax(){
+// 		if (fromtheTop > 0) { 
+// 			if ($('html').hasClass('csstransforms3d')) {
+// 				TweenMax.to(bg, 0.2, {backgroundPosition:'center '+newPos+'px'});						
+// 			}
+// 		} else { 
+// 			TweenMax.set(bg, {backgroundPosition:'center 0px'});						
+// 		}
+// 	}
 
-	TweenMax.ticker.addEventListener("tick", parallax);
+// 	TweenMax.ticker.addEventListener("tick", parallax);
 
-	$(window).scroll( function() { 
-		fromtheTop = $(this).scrollTop();
-		newPos = ( (fromtheTop * 0.25) + 0); 
-	});
+// 	$(window).scroll( function() { 
+// 		fromtheTop = $(this).scrollTop();
+// 		newPos = ( (fromtheTop * 0.25) + 0); 
+// 	});
 
-});
+// });
